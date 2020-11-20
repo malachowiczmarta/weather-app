2e96de2def8002c5c52b378a134751db4ac6aae7
Marta Małachowicz <malachowicz.marta@gmail.com>
2020-11-20 14:41:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const CITY = 'Gdansk';
+const UNITS = 'metric';
+const APP_ID = 'b393c8fbc50415e7b4168a4553a825d3'
+const URL = `${API_URL}?q=${CITY}&units=${UNITS}&appid=${APP_ID}`
+
+class App extends React.Component {
+
+  state = {
+    temperature: '',
+    icon: "",
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+      temperature: data.main.temp,
+      icon: data.weather[0].icon
+    });
+    });
+  }
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {CITY} {this.state.temperature}
+       <img src={`https://openweathermap.org/img/wn/${this.state.icon}@2x.png`}></img>
     </div>
   );
+  }
+  
 }
 
 export default App;
